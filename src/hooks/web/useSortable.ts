534b24dff2fd5b3c024ab7621428eed501b069c3
wangqz <2809755637@qq.com>
2024-04-17 15:03:18 +0800

    fix(*):移除无用的文件

--- src/hooks/web/useSortable.ts
+++ src/hooks/web/useSortable.ts
@@ -1,6 +1,5 @@
 import { nextTick, unref } from 'vue';
 import type { Ref } from 'vue';
-import type { Options } from 'sortablejs';
 
 export function useSortable(el?: HTMLElement | Ref<HTMLElement | undefined>, options?: Options) {
   function initSortable() {
@@ -9,13 +8,7 @@ export function useSortable(el?: HTMLElement | Ref<HTMLElement | undefined>, opt
 
       if (!el) return;
 
-      const Sortable = (await import('sortablejs')).default;
-      Sortable.create(el, {
-        animation: 100,
-        delay: 400,
-        delayOnTouchOnly: true,
-        ...options,
-      });
+    
     });
   }
 
